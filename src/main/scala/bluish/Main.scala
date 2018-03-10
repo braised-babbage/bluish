e052c1c33f41165197c50074402d93d3d127398b
Erik Davis <erik@Eriks-MacBook-Pro.local>
2018-03-10 14:11:49 -0600

    refactor ui code

--- src/main/scala/bluish/Main.scala
+++ src/main/scala/bluish/Main.scala
@@ -5,13 +5,13 @@ import dom.document
 import org.scalajs.dom.html
 import scala.util.Random
 
-case class KeyState(arrowLeft: Boolean, arrowRight: Boolean, arrowUp: Boolean)
+
 
 @JSExportTopLevel("ScalaJSExample")
 object ScalaJSExample {
 
 
-  def update(s: State, dt: Double): State = {
+  def update(s: State, dt: Double, keys: KeyState): State = {
     val actors = s.actors.map(a => a.update(dt, s, keys))
     var newState = State(s.level, actors, s.status)
 
@@ -29,23 +29,6 @@ object ScalaJSExample {
       }
     }
     newState
-  }
-
-  var keys = KeyState(false, false, false)
-
-  def track(e: dom.KeyboardEvent) = {
-    if (e.key == "ArrowLeft") {
-      keys = KeyState(e.`type` == "keydown", keys.arrowRight, keys.arrowUp)
-      e.preventDefault()
-    }
-    if (e.key == "ArrowRight") {
-      keys = KeyState(keys.arrowLeft, e.`type` == "keydown", keys.arrowUp)
-      e.preventDefault()
-    }
-    if (e.key == "ArrowUp") {
-      keys = KeyState(keys.arrowLeft, keys.arrowRight, e.`type` == "keydown")
-      e.preventDefault()
-    }
   }
 
 
@@ -82,58 +65,22 @@ object ScalaJSExample {
 ................................................................................
         """)
 
-
-    val display = Graphics.createElement(
-      "div",
-      Map("class" -> "game"),
-      List(Graphics.drawGrid(level)))
-
-    document.body.appendChild(display)
-
     var state = State(level, actors, Playing)
-    var actorLayer = Graphics.drawActors(state.actors)
-    display.appendChild(actorLayer)
-
-
-    def scrollPlayerIntoView(state: State): Unit = {
-      val width = display.clientWidth
-      val height = display.clientHeight
-      val margin = width / 3
-
-      val left = display.scrollLeft
-      val right = left + width
-      val top = display.scrollTop
-      val bottom = top + height
-
-      val player = state.player
-      val center = (player.pos + player.size * 0.5)*Graphics.scale
-
-      if (center.x < left + margin)
-        display.scrollLeft = center.x - margin
-      else if (center.x > right - margin)
-        display.scrollLeft = center.x + margin - width
-      
-
-      if (center.y < top + margin) 
-        display.scrollTop = center.y - margin
-      else if (center.y > bottom - margin)
-        display.scrollTop = center.y + margin - height
-    }
 
 
     val dt = 20.0 // milliseconds
+    val ui = new DomUI()
+    ui.draw(state)
 
     def run() = {
-      display.removeChild(actorLayer)
-      state = update(state, dt/1000)
-      actorLayer = Graphics.drawActors(state.actors)
-      display.appendChild(actorLayer)
-      display.setAttribute("class",Graphics.cssClass(state.status))
-      scrollPlayerIntoView(state)
-    }
 
-    dom.window.addEventListener("keydown", track _)
-    dom.window.addEventListener("keyup", track _)
+      // clear foreground
+      // update state
+      // draw
+      state = update(state, dt/1000, ui.keys)
+      ui.update(state)
+      ui.scrollPlayerIntoView(state)
+    }
 
     dom.window.setInterval(() => run, dt)
   }
---
+++ src/main/scala/bluish/UI.scala
@@ -0,0 +1,150 @@
+package bluish
+
+import org.scalajs.dom
+import dom.document
+import org.scalajs.dom.html
+
+
+trait Graphics {
+  //def clear: Unit
+  def draw(state: State): Unit
+  def update(state: State): Unit
+  def scrollPlayerIntoView(state: State): Unit
+}
+
+case class KeyState(arrowLeft: Boolean, arrowRight: Boolean, arrowUp: Boolean)
+
+trait KeyboardInterface {
+  def keys: KeyState
+}
+
+class DomUI extends Graphics with KeyboardInterface {
+  import DomGraphics._
+  val scale = 20
+  val display = createElement("div", Map("class" -> "game"))
+  var foreground: Option[dom.Element] = None
+
+
+  document.body.appendChild(display)
+
+  def draw(s: State) = {
+    display.appendChild(drawGrid(s.level, scale))
+    update(s)
+  }
+
+  def update(state: State): Unit = {
+    for (fg <- foreground) display.removeChild(fg)
+    foreground = Some(drawActors(state.actors,scale))
+    for (fg <- foreground) display.appendChild(fg)
+
+    display.setAttribute("class", cssClass(state.status))
+  }
+
+
+  def scrollPlayerIntoView(state: State): Unit = {
+    val width = display.clientWidth
+    val height = display.clientHeight
+    val margin = width / 3
+
+    val left = display.scrollLeft
+    val right = left + width
+    val top = display.scrollTop
+    val bottom = top + height
+
+    val player = state.player
+    val center = (player.pos + player.size * 0.5)*scale
+
+    if (center.x < left + margin)
+      display.scrollLeft = center.x - margin
+    else if (center.x > right - margin)
+      display.scrollLeft = center.x + margin - width
+
+
+    if (center.y < top + margin)
+      display.scrollTop = center.y - margin
+    else if (center.y > bottom - margin)
+      display.scrollTop = center.y + margin - height
+  }
+
+
+  var keys = KeyState(false, false, false)
+
+  def track(e: dom.KeyboardEvent) = {
+      if (e.key == "ArrowLeft") {
+      keys = KeyState(e.`type` == "keydown", keys.arrowRight, keys.arrowUp)
+      e.preventDefault()
+    }
+    if (e.key == "ArrowRight") {
+      keys = KeyState(keys.arrowLeft, e.`type` == "keydown", keys.arrowUp)
+      e.preventDefault()
+    }
+    if (e.key == "ArrowUp") {
+      keys = KeyState(keys.arrowLeft, keys.arrowRight, e.`type` == "keydown")
+      e.preventDefault()
+    }
+  }
+
+  dom.window.addEventListener("keydown", track _)
+  dom.window.addEventListener("keyup", track _)
+
+}
+
+object DomGraphics {
+  def createElement(name: String, attr: Map[String, String], children: Seq[dom.Node] = Seq()): dom.Element = {
+    val elt = document.createElement(name)
+
+    for ((k,v) <- attr) elt.setAttribute(k, v)
+
+    for (c <- children) elt.appendChild(c)
+
+    elt
+  }
+
+  def cssClass(block: Block): String = block match {
+    case Empty => "empty"
+    case Wall => "wall"
+    case Lava => "lava"
+  }
+
+  def cssClass(status: GameStatus): String = status match {
+    case Playing => "game playing"
+    case Won => "game won"
+    case Lost => "game lost"
+  }
+
+  def cssClass(actor: Actor): String = "actor " + (actor match {
+    case p: Player => "player"
+    case c: Coin => "coin"
+    case m: MovingLava => "lava"
+  })
+
+  def drawGrid(level: Level, scale: Int): dom.Element = {
+    val blocks =
+      for (row <- level.background)
+      yield createElement(
+        "tr",
+        Map("height" -> s"${scale}px"),
+        row.map(block => createElement("td", Map("class" -> cssClass(block))))
+      )
+
+    createElement("table",
+      Map("class" -> "background", "style" -> s"width: ${level.width * scale}px"),
+      blocks)
+  }
+
+  def style(actor: Actor, scale: Int): String =
+    s"width: ${actor.size.x * scale}px; height: ${actor.size.y * scale}px; left: ${actor.pos.x * scale}px; top: ${actor.pos.y * scale}px;"
+
+  def drawActors(actors: Seq[Actor], scale: Int): dom.Element = {
+    val elts =
+      for (actor <- actors) yield {
+       createElement("div",
+         Map(
+           "class" -> cssClass(actor),
+           "style" -> style(actor, scale)
+         )
+       )
+      }
+    createElement("div", Map(), elts)
+  }
+}
